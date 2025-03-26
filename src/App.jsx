a4e54791091956a8b21f78da31e3fdf92bbfbca6
Nicole-Villon-Alejandro <nicolevillon5@gmail.com>
2025-03-26 04:28:02 +0100

    add:loader_and_globalContext

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,32 @@ import DefaultLayout from "./layouts/DefaultLayout"
 import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import Homepage from "./pages/Homepage";
 import Moviepage from "./pages/Moviepage";
-import CreateMoviePage from './pages/CreateMoviePage'
+import CreateMoviePage from './pages/CreateMoviePage';
+import GlobalContext from "./contexts/globalContext";
+import { useState } from "react";
 
 
 function App() {
-  //const [count, setCount] = useState(0)
+
+  const [isLoading, setIsLoading ] = useState(false);
+  
 
   return (
     <>
-      <BrowserRouter>
+      <GlobalContext.Provider value={{isLoading, setIsLoading }}>
+       <BrowserRouter>
+         <Routes>
+           <Route Component={DefaultLayout}>
+             <Route path="/" element={<Homepage/>} />
+             <Route path="/movies/:id" element={<Moviepage/>} />
+             <Route path="/movies/create" element={<CreateMoviePage/>} />
+           </Route>
+         </Routes>
+        </BrowserRouter>
         
-        <Routes>
-          <Route Component={DefaultLayout}>
-            <Route path="/" element={<Homepage/>} />
-            <Route path="/movies/:id" element={<Moviepage/>} />
-            <Route path="/movies/create" element={<CreateMoviePage/>} />
-          </Route>
-        </Routes>
-        
-      </BrowserRouter>
+      </GlobalContext.Provider>
+
+      
       
       
       
